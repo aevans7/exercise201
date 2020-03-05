e2c1c05f112d4c82498414d4515ab2688514c218
aevans7 <alex.c.evans1@gmail.com>
2020-03-04 20:52:59 -0500

    Initial commit

---
+++ src/main/java/com/ae/exercise201/Exercise201Application.java
@@ -0,0 +1,13 @@
+package com.ae.exercise201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise201Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ae/exercise201/Exercise201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.exercise201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
